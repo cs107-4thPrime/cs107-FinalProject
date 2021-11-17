d67ce7a162db619368460fcc97966852cbd9fd9a
xiwenjia <xiwenjia2019@gmail.com>
2021-11-17 18:10:49 -0500

    added logistic function and reorder & group the elementary functions

--- src/Derivatives.py
+++ src/Derivatives.py
@@ -9,40 +9,9 @@ Original file is located at
 
 import math
 import numpy as np
-from Dual_class import Dual
+from src.Dual_class import Dual
 
-def ln(a: Dual) -> Dual:
-  """Natural log of Dual number a, using math.log(x)"""
-  value = math.log(a.value)
-  ders = dict()
-  for k,v in a.ders.items():
-    ders[k] = 1/(a.value) * v
-  return Dual(value, ders)
-
-def log(a: Dual, base: int) -> Dual:
-  """Log of Dual number a with base n, using math.log(x, base)"""
-  value = math.log(a.value, base)
-  ders = dict()
-  for k,v in a.ders.items():
-    ders[k] = 1/(a.value * math.log(base))*v
-  return Dual(value, ders)
-
-def exp(a: Dual) -> Dual:
-  """Exponential of the dual number a, using math.exp(x)"""
-  value = math.exp(a.value)
-  ders = dict()
-  for k,v in a.ders.items():
-    ders[k] = math.exp(a.value)*v
-  return Dual(value, ders)  
-
- def power(a: Dual, p: int or float) -> Dual:
-   """power (dual number) of base p (integer or float number), using math.power(x, y)"""
-   """test for the power(a: Dual, p: int or float) function is not yet implemented in test_derivatives.py"""
-   value = math.power(p, a)
-   ders = dict()
-   for k,v in a.ders.items():
-     ders[k] = a*(p**(a-1))*v
-   return Dual(value, ders)
+# Trig functions
 
 def sin(a: Dual) -> Dual:
   """sine of the dual number a, using math.sin(x) and math.cos(x)"""
@@ -67,6 +36,9 @@ def tan(a: Dual) -> Dual:
   for k,v in a.ders.items():
     ders[k] = 1/((math.cos(a.value))**2)*v
   return Dual(value, ders)
+
+
+# Inverse trig functions
 
 def asin(a: Dual) -> Dual:
   """inverse of sine or arcsine of the dual number a, using math.asin(x)"""
@@ -99,31 +71,97 @@ def atan(a: Dual) -> Dual:
     ders[k] = 1/(1+(a.value)**2)*v
   return Dual(value, ders)
 
- def sinh(a: Dual) -> Dual:
-   """sinh of the dual number a, using math.sinh(x) and math.cosh(x)"""
-   """test for the sinh(a: Dual) function is not yet implemented in test_derivatives.py"""
-   value = math.sinh(a.value)
-   ders = dict()
-   for k,v in a.ders.items():
-     ders[k] = math.cosh(a.value)*v
-   return Dual(value, ders)
 
- def cosh(a: Dual) -> Dual:
-   """cosh of the dual number a, using math.cosh(x)"""
-   """test for the cosh(a: Dual) function is not yet implemented in test_derivatives.py"""
-   value = math.cosh(a.value)
-   ders = dict()
-   for k,v in a.ders.items():
-     ders[k] = math.sinh(a.value)*v
-   return Dual(value, ders)
+# Exponential functions
 
- def tanh(a: Dual) -> Dual:
-   """tanh of the dual number a, using math.tanh(x)"""
-   """test for the tanh(a: Dual) function is not yet implemented in test_derivatives.py"""
-   value = math.tanh(a.value)
-   ders = dict()
-   for k,v in a.ders.items():
-     ders[k] = 1/(math.cosh(a.value)**2)*v
+def exp(a: Dual) -> Dual:
+  """Exponential of the dual number a, using math.exp(x)"""
+  value = math.exp(a.value)
+  ders = dict()
+  for k,v in a.ders.items():
+    ders[k] = math.exp(a.value)*v
+  return Dual(value, ders)  
+
+def power(a: Dual, p: int or float) -> Dual:
+  """power (dual number) of base p (integer or float number), using math.power(x, y)"""
+  """test for the power(a: Dual, p: int or float) function is not yet implemented in test_derivatives.py"""
+  value = math.power(p, a)
+  ders = dict()
+  for k,v in a.ders.items():
+    ders[k] = a*(p**(a-1))*v
+  return Dual(value, ders)
+
+
+# Hyperbolic functions
+
+def sinh(a: Dual) -> Dual:
+  """sinh of the dual number a, using math.sinh(x) and math.cosh(x)"""
+  """test for the sinh(a: Dual) function is not yet implemented in test_derivatives.py"""
+  value = math.sinh(a.value)
+  ders = dict()
+  for k,v in a.ders.items():
+    ders[k] = math.cosh(a.value)*v
+  return Dual(value, ders)
+
+def sinh(a: Dual) -> Dual:
+  """sinh of the dual number a, using math.sinh(x) and math.cosh(x)"""
+  """test for the sinh(a: Dual) function is not yet implemented in test_derivatives.py"""
+  value = math.sinh(a.value)
+  ders = dict()
+  for k,v in a.ders.items():
+    ders[k] = math.cosh(a.value)*v
+  return Dual(value, ders)
+
+def cosh(a: Dual) -> Dual:
+  """cosh of the dual number a, using math.cosh(x)"""
+  """test for the cosh(a: Dual) function is not yet implemented in test_derivatives.py"""
+  value = math.cosh(a.value)
+  ders = dict()
+  for k,v in a.ders.items():
+    ders[k] = math.sinh(a.value)*v
+  return Dual(value, ders)
+
+def tanh(a: Dual) -> Dual:
+  """tanh of the dual number a, using math.tanh(x)"""
+  """test for the tanh(a: Dual) function is not yet implemented in test_derivatives.py"""
+  value = math.tanh(a.value)
+  ders = dict()
+  for k,v in a.ders.items():
+    ders[k] = 1/(math.cosh(a.value)**2)*v
+  return Dual(value, ders)
+
+
+# Logistic functions
+
+def logistic(a: Dual) -> Dual:
+  """logistic function of the dual number a, using math.exp(x)"""
+  value = 1/(1+math.exp(-a))
+  ders = dict()
+  for k,v in a.ders.items():
+    ders[k] = (math.exp(a)/((1 + math.exp(a))**2))*v
+  return Dual(value, ders)
+  
+
+# Logarithms
+
+def ln(a: Dual) -> Dual:
+  """Natural log of Dual number a, using math.log(x)"""
+  value = math.log(a.value)
+  ders = dict()
+  for k,v in a.ders.items():
+    ders[k] = 1/(a.value) * v
+  return Dual(value, ders)
+
+def log(a: Dual, base: int) -> Dual:
+  """Log of Dual number a with base n, using math.log(x, base)"""
+  value = math.log(a.value, base)
+  ders = dict()
+  for k,v in a.ders.items():
+    ders[k] = 1/(a.value * math.log(base))*v
+  return Dual(value, ders)
+
+
+# Square root
 
 def sqrt(a: Dual) -> Dual:
   """square root of a dual number"""
